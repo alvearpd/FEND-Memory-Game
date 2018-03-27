44200f87942dde97d814b9fcf691fb25c0a91c4a
elharony <p.yahyaelharony@gmail.com>
2018-03-27 09:45:47 +0200

    Create the Cards List

---
+++ js/app.js
@@ -0,0 +1,12 @@
+/* 
+ * Create HTML List
+ *
+ */
+const gameContainer = document.querySelector(".game-container");
+
+/*
+ * Create Cards List
+ * Add it to the Game Container
+ */
+const cardsList = document.createElement("ul");
+gameContainer.appendChild(cardsList);
