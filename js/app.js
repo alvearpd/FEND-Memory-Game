9d85aadfe8e888c5b27ce0f2cf2b7a9d3ba1d89d
elharony <p.yahyaelharony@gmail.com>
2018-03-28 23:14:31 +0200

    Add unshuffled icons list

--- js/app.js
+++ js/app.js
@@ -2,3 +2,8 @@
  * CardsList
  */
 const cardsList = document.querySelector(".cards");
+
+/*
+ * Icons
+ */
+const icons = ["fab fa-apple", "fab fa-apple", "fas fa-ban", "fas fa-ban", "far fa-bell", "far fa-bell", "fab fa-bluetooth-b", "fab fa-bluetooth-b", "fas fa-camera-retro", "fas fa-camera-retro", "fas fa-car", "fas fa-car", "fas fa-chess", "fas fa-chess", "far fa-envelope", "far fa-envelope"];
