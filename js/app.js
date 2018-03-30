a22bec4d7ea142b959cc4ff348e8af69aa3fbc33
elharony <p.yahyaelharony@gmail.com>
2018-03-30 17:50:02 +0200

    Add more icons

--- js/app.js
+++ js/app.js
@@ -6,9 +6,7 @@ const cardsList = document.querySelector(".cards");
 /*
  * Icons
  */
-const iconsList = ["fab fa-apple", "fab fa-apple", "fas fa-ban", "fas fa-ban", "far fa-bell", "far fa-bell", "fab fa-bluetooth-b", "fab fa-bluetooth-b"];
-
-// , "fas fa-camera-retro", "fas fa-camera-retro", "fas fa-car", "fas fa-car", "fas fa-chess", "fas fa-chess", "far fa-envelope", "far fa-envelope"
+const iconsList = ["fab fa-apple", "fab fa-apple", "fas fa-ban", "fas fa-ban", "far fa-bell", "far fa-bell", "fab fa-bluetooth-b", "fab fa-bluetooth-b", "fas fa-camera-retro", "fas fa-camera-retro", "fas fa-car", "fas fa-car", "fas fa-chess", "fas fa-chess", "far fa-envelope", "far fa-envelope"];
 
 /*
  * Shuffle
