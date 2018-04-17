fb4fc99f58d2d7165062b1d0fccbb77d027df9cf
elharony <p.yahyaelharony@gmail.com>
2018-04-17 23:52:14 +0200

    refactor: Change the long code to small functions
    This refactor includes:
    - Rename functions
    - Wrap the variables together
    - Delete unnecessary condition
    - Divide the long code into small functions

--- js/app.js
+++ js/app.js
@@ -3,7 +3,13 @@
  */
 let allOpenCards     = [],
     currentOpenCards = [],
-    moves            = 0;
+    moves            = 0,
+    rateHTML = "",
+    rateStep = 6,
+    firstClick = true,
+    hours, minutes, seconds,
+    totalTime = 0,
+    incrementer;
 
 const iconsList          = ["fab fa-apple", "fab fa-apple", "fas fa-ban", "fas fa-ban", "far fa-bell", "far fa-bell", "fab fa-bluetooth-b", "fab fa-bluetooth-b", "fas fa-camera-retro", "fas fa-camera-retro", "fas fa-car", "fas fa-car", "fas fa-chess", "fas fa-chess", "far fa-envelope", "far fa-envelope"],
       cardsList          = document.querySelector(".cards"),
@@ -11,7 +17,15 @@ const iconsList          = ["fab fa-apple", "fab fa-apple", "fas fa-ban", "fas f
       movesContainer     = document.querySelector(".moves"),
       modal              = document.querySelector(".modal"),
       repeatBtn          = document.querySelector(".features .play-again");
-      repeatBtnFromModal = document.querySelector(".modal .play-again");
+      repeatBtnFromModal = document.querySelector(".modal .play-again"),
+      rateContainer      = document.querySelector("#total_rate"),
+      exactMoves         = iconsList.length / 2,
+      maxStars = exactMoves + rateStep,
+      minStars = exactMoves + ( 2 * rateStep),
+      stars    = document.querySelectorAll(".star"),
+      secondsContainer = document.querySelector("#seconds"),
+      minutesContainer = document.querySelector("#minutes"),
+      hoursContainer   = document.querySelector("#hours"); 
 
 /*
  * Shuffle
@@ -37,9 +51,9 @@ function shuffle(array) {
 }
 
 /*
- * Start Game
+ * Init Game
  */
-function start() {
+function init() {
     const icons = shuffle(iconsList);
     const cardsFragment = document.createDocumentFragment();
     for (let i = 0; i < icons.length; i++) {
@@ -49,11 +63,20 @@ function start() {
         cardsFragment.appendChild(card);
     }
     cardsList.appendChild(cardsFragment);
+}
 
+/*
+ * Start Game
+ */
+function start() {
+    
+    // Initialize Game
+    init();
+
+    // Click Functionality
     cardClick();
 }
 
-let firstClick = true;
 /* 
 * Click
 */
@@ -74,69 +97,71 @@ function cardClick() {
                 firstClick = false;
             }
 
-            // If this element exist in `allOpenCards` arr
-            if (!allOpenCards.includes(currentCard)) {
+            // Check if there is 1 open card
+            if (currentOpenCards.length === 1) {
 
-                // Check if there are 2 open cards
-                if (currentOpenCards.length === 1) {
-
-                    // If the user clicked on the same card
-                    if (currentCard.id !== previousCard.id) {
-
-                        // Open it, Add it to the `currentOpenCards` arr
-                        currentCard.className = "show";
-                        currentOpenCards.push(currentCard);
-
-                        // Compare the current Card with the current one in `currentOpenCards` arr
-                        if (currentCard.childNodes[0].className === previousCard.childNodes[0].className) {
-
-                            // Correct highlight
-                            currentCard.style.backgroundColor = "green";
-                            previousCard.style.backgroundColor = "green";
-                            currentCard.className = "animated flash disabled";
-                            previousCard.className = "animated flash disabled";
-
-                            // Add Current & Previous card to `allOpenCards` to compare it with the original one to determine if the game is over
-                            allOpenCards.push(currentCard.childNodes[0].className, previousCard.childNodes[0].className);
-
-                            // Check over
-                            checkOver();
-                        } else {
-
-                            // Back to normal
-                            setTimeout(function () {
-                                currentCard.className = "animated jello";
-                                previousCard.className = "animated jello";
-                                currentCard.style.backgroundColor = "#333";
-                                currentCard.childNodes[0].style.opacity = "0";
-                                previousCard.style.backgroundColor = "#333";
-                                previousCard.childNodes[0].style.opacity = "0";
-                            }, 500)
-
-                        }
-
-                        // Empty `currentOpenCards`
-                        currentOpenCards = [];
-
-                        // Add Move
-                        addMove();
-
-                        // Rating
-                        rating();
-                    }
-
+                // If the user clicked on the same card
+                if (currentCard.id !== previousCard.id) {
 
                     // Open it, Add it to the `currentOpenCards` arr
-                } else {
                     currentCard.className = "show";
                     currentOpenCards.push(currentCard);
-                }
-            } else {
 
+                    // Compare the current Card with the current one in `currentOpenCards` arr
+                    isMatched(currentCard, previousCard);
+
+                    // Empty `currentOpenCards`
+                    currentOpenCards = [];
+
+                    // Add Move
+                    addMove();
+
+                    // Rating
+                    rating();
+                }
+
+                // Open it, Add it to the `currentOpenCards` arr
+            } else {
+                currentCard.className = "show";
+                currentOpenCards.push(currentCard);
             }
         });
     }
 }
+
+/*
+ * Is Matched?
+ */
+function isMatched(currentCard, previousCard) {
+    if (currentCard.childNodes[0].className === previousCard.childNodes[0].className) {
+
+        // Correct highlight
+        currentCard.style.backgroundColor = "green";
+        previousCard.style.backgroundColor = "green";
+        currentCard.className = "animated flash disabled";
+        previousCard.className = "animated flash disabled";
+
+        // Add Current & Previous card to `allOpenCards` to compare it with the original one to determine if the game is over
+        allOpenCards.push(currentCard.childNodes[0].className, previousCard.childNodes[0].className);
+
+        // Game Over?
+        isOver();
+
+    } else {
+
+        // Back to normal
+        setTimeout(function () {
+            currentCard.className = "animated jello";
+            previousCard.className = "animated jello";
+            currentCard.style.backgroundColor = "#333";
+            currentCard.childNodes[0].style.opacity = "0";
+            previousCard.style.backgroundColor = "#333";
+            previousCard.childNodes[0].style.opacity = "0";
+        }, 500)
+
+    }
+}
+
 
 /*
  * Add Move
@@ -148,9 +173,9 @@ function addMove() {
 
 
 /*
- * Check Game Over
+ * Game Over?
  */
-function checkOver() {
+function isOver() {
     if (iconsList.length === allOpenCards.length) {
         gameOverMessage();
     }
@@ -207,23 +232,6 @@ repeatBtnFromModal.addEventListener("click", function () {
 /* 
  * Star Rating
  */
-
-// Exact Moves
-const exactMoves = iconsList.length / 2;
-
-// All Moves
-// `moves`
-
-// Rate 
-const rateContainer = document.querySelector("#total_rate");
-let rateHTML = "";
-
-const rateStep = 3;
-const maxStars = exactMoves + rateStep;
-const minStars = exactMoves + ( 2 * rateStep);
-
-const stars    = document.querySelectorAll(".star");
-
 function rating() {
 
     if(moves < maxStars) {
@@ -236,17 +244,7 @@ function rating() {
         rateHTML = "<i class='star fas fa-star'></i>";
     }
 
-}
-
- 
-/*
- * Timer [ Variables ] 
- */
-const secondsContainer = document.querySelector("#seconds"),
-      minutesContainer = document.querySelector("#minutes"),
-      hoursContainer   = document.querySelector("#hours");
-let totalTime = 0;
-let incrementer;
+} 
 
 /*
  * Timer [ Start ] 
@@ -275,12 +273,10 @@ function startTimer() {
 /*
  * Timer [ Calculate Time ] 
  */
-let hours, minutes, seconds; 
 function calculateTime(totalTime) {
     hours   = Math.floor( totalTime / 60 / 60);
     minutes = Math.floor( (totalTime / 60) % 60);
     seconds = totalTime % 60;
-
 }
 
 /*
